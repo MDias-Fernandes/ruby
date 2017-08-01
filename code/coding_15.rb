4575991d6d35712cd486331b2d9537b11c769c82
MDias-Fernandes <diasmatehus621@gmail.com>
2017-07-31 22:13:26 -0300

    Learn Ruby

---
+++ code/coding_15.rb
@@ -0,0 +1,8 @@
+#Working with algorithms and logical operators
+
+# Parenthesis | Exponents | Multiplication | Division | Addition | Subtraction |
+# (some math) |   6**5    |       6*5      |   6/5    |   6+5    |    6-5      |
+
+# if (x > 7 && y < 8)       => AND
+# if (x > 7 || y < 8)       => OR
+# if (x>7 && y<8) || a==9   => first AND, then OR
