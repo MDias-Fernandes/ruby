abf1758b813919ece8a91ad3e3b8b8635d689642
Matheus Dias <diasmatheus621@gmail.com>
2017-12-15 16:40:36 -0200

    Update find_file.rb

--- annotations/find_file.rb
+++ annotations/find_file.rb
@@ -6,3 +6,15 @@ puts __FILE__                                                       #filename of
 puts "---------------"
 puts File.expand_path(File.dirname(__FILE__)) << ("/../upload.png") #filename from a file defined
 puts "---------------"
+
+#split text from a file (.txt) and map every element until the desired element be found.
+File.open('C:\Users\user_name\Downloads\document_txt.txt', 'r') do |find_url|
+    find_url.each_line do |line|
+        array_from_email = line.split
+        array_from_email.map do |element|
+            if element.include? ('http://www.')
+            puts element
+            end
+        end
+    end
+end
