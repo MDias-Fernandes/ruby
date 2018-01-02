57decb137ef9c216fe2d589692c1ca9adcbb0dd3
MDias-Fernandes <diasmatehus621@gmail.com>
2018-01-02 06:57:28 -0200

    To start 13

---
+++ secao_12/!_ip_validation.rb
@@ -0,0 +1,8 @@
+IP_ADDRESS_REGEX = /^((?:(?:^|\.)(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])){4})$/
+
+def is_valid_ip_address? ip
+    ip = IP_ADDRESS_REGEX
+end
+
+puts is_valid_ip_address? ("999.16.0.0") ? "Valid" : "Invalid"
+puts is_valid_ip_address? ("172.16.0.111") ? "Valid" : "Invalid"
---
+++ secao_12/wheel_of_fortune.rb
@@ -0,0 +1,25 @@
+starting_sentence = "Hi from matching land"
+sentence_array = starting_sentence.split(//).map(&:downcase)
+#["H", "i", " ", "F", "r", "o", "m", ...]
+account_count = sentence_array - [" "]
+#How much index minus the empty spaces on it
+#With empty spaces = 20 indexes
+#Without empty spaces = 17 indexes
+final_sentence = starting_sentence.gsub(/[a-zA-Z]/, "_").split(//)
+#Subtitute all the caracters for "_" and split it like the line 3, but with "_"
+
+
+while sentence_array.count("") < account_count.count
+    puts "Guess a letter:"
+    guess = gets.downcase.chomp
+
+    if sentence_array.include?(guess)
+        letter_index = sentence_array.find_index(guess)
+        sentence_array[letter_index] = ""
+        final_sentence[letter_index] = guess
+        puts "Correct! The sentence: #{final_sentence.join}"
+    else
+        puts "Sorry, try again!"
+    end
+end
+# FAQ - REVIEW lesson 62
